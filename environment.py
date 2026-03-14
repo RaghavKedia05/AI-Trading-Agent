bafc1ca739b06629b245bcab9690fbe09f698c7c
Raghav Kedia <raghavkedia2205@gmail.com>
2026-03-14 16:37:22 +0530

    Implement TradingEnvironment class for trading logic

---
+++ environment.py
@@ -0,0 +1,40 @@
+from utils import get_state
+
+class TradingEnvironment:
+
+    def __init__(self, data):
+        self.data = data
+        self.initial_balance = 10000
+        self.balance = self.initial_balance
+        self.holdings = 0
+        self.index = 0
+
+    def reset(self):
+        self.balance = self.initial_balance
+        self.holdings = 0
+        self.index = 0
+        return get_state(self.data, self.index)
+
+    def step(self, action):
+
+        price = float(self.data.loc[self.index, 'Close'])
+        reward = 0
+
+        if action == 1 and self.balance >= price:
+            self.holdings = self.balance // price
+            self.balance -= self.holdings * price
+
+        elif action == 2 and self.holdings > 0:
+            self.balance += self.holdings * price
+            self.holdings = 0
+
+        self.index += 1
+
+        done = self.index >= len(self.data) - 1
+
+        if done:
+            reward = self.balance - self.initial_balance
+
+        next_state = None if done else get_state(self.data, self.index)
+
+        return next_state, reward, done, {}
