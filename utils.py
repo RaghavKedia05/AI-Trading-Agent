a3d779b65717242a19c3543a291fc5194d3d2e47
Raghav Kedia <raghavkedia2205@gmail.com>
2026-03-14 16:36:46 +0530

    Add get_state function to extract data as array

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def get_state(data, index):
+    return np.array([
+        float(data.loc[index, 'Close']),
+        float(data.loc[index, 'SMA_5']),
+        float(data.loc[index, 'SMA_20']),
+        float(data.loc[index, 'Returns'])
+    ])
